cc96b78b03d001352e545d4458b8013d69e4e170
Abhishek <pundirabhishek987@gmail.com>
2022-11-25 08:08:32 +0530

    Add quiz.html

---
+++ quiz.js
@@ -0,0 +1,33 @@
+const formQuiz = document.querySelector(".form-quiz");
+const btnSubmit = document.querySelector("#submit-btn");
+const outputResult = document.querySelector("#output");
+
+const correctAnswers = [
+    "3",
+    "180˚",
+    "Obtuse",
+    "Pythagoras",
+    "Equilateral",
+    "Scalene",
+    "Isosceles",
+    "70°",
+    "1",
+    "60°",
+    "Acute",
+]
+
+btnSubmit.addEventListener("click", finalScore);
+
+function finalScore() {
+    const finalResults = new FormData(formQuiz);
+    var score = 0;
+    var index = 0;
+    for (var value of finalResults.values()) {
+        if (value == correctAnswers[index]) {
+            score++;
+            
+        }
+        index = index + 1;
+    } 
+    outputResult.innerText = "Your final score is " + score;
+}
